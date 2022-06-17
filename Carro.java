07d5aa33112d7fb4abba9d5c67e1319d01ed2e57
Cauã Marinho de Sousa <95374270+MarinhoCM@users.noreply.github.com>
2022-06-17 18:07:30 -0300

    Update Code

--- Carro.java
+++ Carro.java
@@ -1,19 +1,4 @@
 // @author caua_marinho
-
-/*Implemente uma classe Carro com as seguintes propriedades: um veículo tem um certo
-consumo de combustível e uma certa quantidade de combustível no tanque. O consumo
-é especificado no construtor e o nível de combustível inicial é 0. Forneça um método
-andar que simule o ato de dirigir o veículo por uma certa distância, reduzindo o nível de
-combustível no tanque de gasolina, e os métodos obterGasolina, que devolvam o nível
-atual de combustível, e adicionarGasolina, para abastecer o tanque.
- 
-Exemplo de uso:
-Carro ferrari = new Carro(5); // Consumo de 5 km/l
-ferrari.adicionarGasolina(50); // Abastece 50 litros
-ferrari.andar(10); // Anda 10 km
-System.out.println("Tanque Ferrari:" + ferrari.obterGasolina());
-// Imprime o combustível que resta.
-*/
 package carro;
 public class Carro {
     private double ConsumoDecombustivel;
